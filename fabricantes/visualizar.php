09af194f80d4958fabb2d18cf22441e1f7c7821e
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-08-24 08:54:33 -0300

    Adicionar pasta fabricante

---
+++ fabricantes/visualizar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabricantes - Visualização</title>
+</head>
+<body>
+    <h1>Fabricantes | SELECT - <a href="../index.PHP">Home</a></h1>
+    <hr>
+    <h2>Lendo e carregando todos os fabricantes.</h2>
+
+    <p><a href="inserir.php">Inserir novo fabricante</a></p>
+</body>
+</html>
